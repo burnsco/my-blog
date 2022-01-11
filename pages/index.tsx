ac9cd54384f09fc0abd6f9f01a7525eb5529dfc8
Corey Burns <coreymburns@gmail.com>
2022-01-11 16:17:51 -0500

    updated deps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
-import Head from "next/head"
-import Container from "../components/container"
-import HeroPost from "../components/hero-post"
-import Intro from "../components/intro"
-import Layout from "../components/layout"
-import MoreStories from "../components/more-stories"
-import { getAllPosts } from "../lib/api"
-import Post from "../types/post"
+import Container from '../components/container'
+import MoreStories from '../components/more-stories'
+import HeroPost from '../components/hero-post'
+import Intro from '../components/intro'
+import Layout from '../components/layout'
+import { getAllPosts } from '../lib/api'
+import Head from 'next/head'
+import { CMS_NAME } from '../lib/constants'
+import Post from '../types/post'
 
 type Props = {
   allPosts: Post[]
@@ -18,7 +19,7 @@ const Index = ({ allPosts }: Props) => {
     <>
       <Layout>
         <Head>
-          <title>Corey Burns' Blog</title>
+          <title>Next.js Blog Example with {CMS_NAME}</title>
         </Head>
         <Container>
           <Intro />
@@ -42,7 +43,14 @@ const Index = ({ allPosts }: Props) => {
 export default Index
 
 export const getStaticProps = async () => {
-  const allPosts = getAllPosts(["title", "date", "slug", "author", "coverImage", "excerpt"])
+  const allPosts = getAllPosts([
+    'title',
+    'date',
+    'slug',
+    'author',
+    'coverImage',
+    'excerpt',
+  ])
 
   return {
     props: { allPosts },
